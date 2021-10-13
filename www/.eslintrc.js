37729cf989aecfc44ae2ee1ad8aa8cf5689a01fc
jhoyberrocal <jberrocal@robinfood.com>
2021-10-13 12:34:44 -0500

    fix require vars

--- www/.eslintrc.js
+++ www/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off',
+    '@typescript-eslint/no-var-requires': 0,
   },
   'settings': {
     'react': {
